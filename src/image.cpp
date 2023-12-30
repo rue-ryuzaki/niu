d387bed83005e71aad0f906123137be11ed7aaf9
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2023-12-30 15:12:32 +0300

    Add: GCC diagnostic ignored -Wsign-conversion

--- src/image.cpp
+++ src/image.cpp
@@ -12,6 +12,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 #include <stb_image.h>
 #pragma GCC diagnostic pop
